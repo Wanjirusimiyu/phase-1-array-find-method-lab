826366149b516bf2c57418c0fc2609288f373cc4
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-03-28 23:01:59 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(superbowlRecords) {
+const winningRecord = superbowlRecords.find(record => record.result === 'W');
+ if(winningRecord) {
+    return winningRecord.year;
+} else {
+    return undefined;
+   }
+}
